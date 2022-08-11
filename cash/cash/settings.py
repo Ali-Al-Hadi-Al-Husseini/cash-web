8e20e74852af17776da0e5e3513fb23004ce8801
Ali-Al-Hadi-Al-Husseini <alihadi.alhusseini@gmail.com>
2022-08-11 23:16:39 +0300

    fixed some errors

--- cash/cash/settings.py
+++ cash/cash/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$gb8u^ma^!g@)dscu*=!5*3z^e)p9mr+xg*@($8_16n(yhlos!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.1.33"]
+# ALLOWED_HOSTS = ["192.168.1.33"]
 
 
 # Application definition
